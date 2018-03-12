b14f242751b57bb156435543e6ddbf7604b0bd4b
Inyoung Cho <jindodog89@gmail.com>
2018-03-11 23:51:42 -0700

    Create IsBST.java

---
+++ IsBST.java
@@ -0,0 +1,21 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node class is defined as follows:
+    class Node {
+        int data;
+        Node left;
+        Node right;
+     }
+*/
+    boolean checkBST(Node root) {
+        return checkBSTAux(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+
+    boolean checkBSTAux(Node root, int min, int max){
+        if(root == null)
+            return true;
+        if(root.data <= min || root.data > max)
+            return false;
+        return (checkBSTAux(root.left,min,root.data) && checkBSTAux(root.right,root.data,max));
+    }
+
